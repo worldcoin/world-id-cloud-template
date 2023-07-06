0cf2675e7c152302e28e8f13d6ff9d47a010f3c8
0xPenryn <penryn@pm.me>
2023-07-06 14:46:27 +0200

    fix: handleVerify works now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import type { VerifyReply } from "./api/verify";
 export default function Home() {
 	const onSuccess = (result: ISuccessResult) => {
 		// This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
-		window.alert("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
+		console.log("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
 	};
 
 	const handleProof = async (result: ISuccessResult) => {
@@ -19,21 +19,19 @@ export default function Home() {
 			signal: "",
 		};
 		console.log("Sending proof to backend for verification:\n", JSON.stringify(reqBody)) // Log the proof being sent to our backend for visibility
-		fetch("/api/verify", {
+		const res: Response = await fetch("/api/verify", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(reqBody),
-		}).then((res: Response) => {
-			res.json().then((data: VerifyReply) => {
-				if (res.status == 200) {
-					console.log("Successful response from backend:\n", data); // Log the response from our backend for visibility
-				} else {
-					throw new Error(`Error code ${res.status} (${data.code}): ${data.detail}` ?? "Unknown error."); // Throw an error if verification fails
-				}
-			});
-		});
+		})
+		const data: VerifyReply = await res.json()
+		if (res.status == 200) {
+			console.log("Successful response from backend:\n", data); // Log the response from our backend for visibility
+		} else {
+			throw new Error(`Error code ${res.status} (${data.code}): ${data.detail}` ?? "Unknown error."); // Throw an error if verification fails
+		}
 	};
 
 	return (
@@ -46,7 +44,7 @@ export default function Home() {
 					onSuccess={onSuccess}
 					handleVerify={handleProof}
 					credential_types={[CredentialType.Orb, CredentialType.Phone]}
-					// autoClose
+					autoClose
 				>
 					{({ open }) =>
 						<button className="border border-black rounded-md" onClick={open}>
